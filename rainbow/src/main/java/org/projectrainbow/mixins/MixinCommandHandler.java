a923865c527caca08a7b39a9c43e739ca1290ca8
Florian Stober <Flo.e3@gmx.de>
2016-05-31 17:27:11 +0200

    fix removed commands showing in help

--- rainbow/src/main/java/org/projectrainbow/mixins/MixinCommandHandler.java
+++ rainbow/src/main/java/org/projectrainbow/mixins/MixinCommandHandler.java
@@ -2,6 +2,8 @@ package org.projectrainbow.mixins;
 
 import PluginReference.MC_CommandSenderInfo;
 import PluginReference.MC_CommandSenderType;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.ChatComponentTranslation;
 import net.minecraft.src.CommandBlockLogic;
@@ -15,17 +17,32 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.projectrainbow.ServerWrapper;
+import org.spongepowered.asm.lib.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Mixin(CommandHandler.class)
 public class MixinCommandHandler {
+
+    @Shadow
+    @Final
+    @Mutable
+    private Map<String, ICommand> commandMap;
+
+    {
+        commandMap = new LinkedHashMap<String, ICommand>();
+    }
 
     @Redirect(method = "executeCommand", at = @At(value = "INVOKE", target = "get"))
     private Object caseInsensitiveMapGet(Map map, Object key) {
@@ -74,5 +91,10 @@ public class MixinCommandHandler {
     @Redirect(method = "tryExecute", at = @At(value = "INVOKE", target = "warn", remap = false))
     private void onExceptionIgnoreOldLog(Logger logger, String message) {
 
+    }
+
+    @Redirect(method = "getPossibleCommands", at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "net.minecraft.src.CommandHandler.commandSet:Ljava/util/Set;"))
+    private Set<ICommand> getCommandSet(CommandHandler self) {
+        return Sets.newHashSet(commandMap.values());
     }
 }
