21da4641fc96f88de87eb6fda1d7a7d33d6a66ad
Florian Stober <Flo.e3@gmx.de>
2016-07-27 16:56:41 +0200

    fix bungeecord support

---
+++ rainbow/src/main/java/org/projectrainbow/mixins/MixinCPacketHandshake.java
@@ -0,0 +1,15 @@
+package org.projectrainbow.mixins;
+
+import net.minecraft.network.handshake.client.C00Handshake;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(C00Handshake.class)
+public class MixinCPacketHandshake {
+
+    @ModifyArg(method = "readPacketData", at = @At(value = "INVOKE", target = "net.minecraft.network.PacketBuffer.readStringFromBuffer(I)Ljava/lang/String;"))
+    private int hostNameFieldSize(int oldSize){
+        return Short.MAX_VALUE;
+    }
+}
