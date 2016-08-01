5c030a2a58f6eb556b9c58d6eb6d30396e7f08b3
Isaiah Patton <isaiahpatton1@gmail.com>
2016-08-01 16:34:42 -0400

    Added viewProp();

--- rainbow/src/main/java/ml/rainbowplusplus/utils/ConfigEditer.java
+++ rainbow/src/main/java/ml/rainbowplusplus/utils/ConfigEditer.java
@@ -1,38 +1,71 @@
 package ml.rainbowplusplus.utils;
 
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
+ import java.io.FileNotFoundException;
+ import java.io.FileOutputStream;
+ import java.io.IOException;
+ import java.util.Properties;
 
-public class ConfigEditer {
+ public class ConfigEditer {
+	 /*
+	  How to use:
+	  
+        try{
+            changeProp("test.properties", "testfield", "testtext");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        try{
+            System.out.println("" + viewProp("test.properties", "testfield"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    
+    */
+ 
+ 	public static void changeProp(String file, String field, String text) throws FileNotFoundException {
+ 		FileOutputStream out = new FileOutputStream(file);
+ 		FileInputStream in = new FileInputStream(file);
+ 		Properties props = new Properties();
+ 		try {
+ 			props.load(in);
+ 		} catch (IOException e1) {
+ 			e1.printStackTrace();
+ 		}
+ 		try {
+ 			in.close();
+ 		} catch (IOException e1) {
+ 			e1.printStackTrace();
+ 		}
 
-	public static void changeProp(String file, String field, String text) throws FileNotFoundException {
-		FileOutputStream out = new FileOutputStream(file);
-		FileInputStream in = new FileInputStream(file);
-		Properties props = new Properties();
-		try {
-			props.load(in);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		try {
-			in.close();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-
-		props.setProperty(field, text);
-		try {
-			props.store(out, null);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-}
+ 		props.setProperty(field, text);
+ 		try {
+ 			props.store(out, null);
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		}
+ 		try {
+ 			out.close();
+ 		} catch (IOException e) {
+ 			e.printStackTrace();
+ 		}
+ 	}
+ 	public static String viewProp(String file, String field) throws FileNotFoundException {
+        FileInputStream in = new FileInputStream(file);
+ 		Properties prop = new Properties();
+        try {
+ 			prop.load(in);
+ 		} catch (IOException e1) {
+ 			e1.printStackTrace();
+ 		}
+        try {
+ 			in.close();
+ 		} catch (IOException e1) {
+ 			e1.printStackTrace();
+ 		}
+ 	    return prop.getProperty(field);
+ 	}
+ 	
+ }
