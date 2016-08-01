2038beeedb66c58a5a7d6510e2b16f49a29b3c32
Isaiah Patton <isaiahpatton1@gmail.com>
2016-07-31 20:16:36 -0400

    Create ConfigEditer.java

---
+++ rainbow/src/main/java/ml/rainbowplusplus/utills/ConfigEditer.java
@@ -0,0 +1,36 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigEditer {
+
+	public static void changeProp(String file, String field, String text) throws FileNotFoundException {
+		FileOutputStream out = new FileOutputStream(file);
+		FileInputStream in = new FileInputStream(file);
+		Properties props = new Properties();
+		try {
+			props.load(in);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			in.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		props.setProperty(field, text);
+		try {
+			props.store(out, null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
