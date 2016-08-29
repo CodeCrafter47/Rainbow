f2424687424f443072b22887abe830523e1e9f38
Isaiah Patton <isaiahpatton1@gmail.com>
2016-08-29 18:07:15 -0400

    Update RPPUtils.java

--- rainbow/src/main/java/ml/rainbowplusplus/utils/RPPUtils.java
+++ rainbow/src/main/java/ml/rainbowplusplus/utils/RPPUtils.java
@@ -7,13 +7,16 @@ import org.apache.commons.io.FileUtils;
 import org.projectrainbow.launch.Bootstrap;
 
 public class RPPUtils {
-	File BukkitPluginsloaderJar = new File("plugins_mod" + File.separator + "PluginBukkitBridge.jar");
-	public static void downloadBukkitBridge() {
-    if (BukkitPluginsloaderJar.exists()) {
-        Bootstrap.logger.info("Found BUKKIT tweaker...");
-        Bootstrap.logger.info(" Loaded org.bukkit.Bukkit");
-        Bootstrap.logger.info(" Loaded org.bukkit.plugin.java.PluginClassLoader");
-    } else {
+    File BukkitPluginsloaderJar = new File("plugins_mod" + File.separator + "PluginBukkitBridge.jar");
+    public static void downloadBukkitBridge() {
+    	// Why bukkit?
+    	// Bukkit might be dead, 
+    	// but it still has over 10,000 plugins that are updated to 1.10
+    	if (BukkitPluginsloaderJar.exists()) {
+	    Bootstrap.logger.info("Found BUKKIT tweaker...");
+            Bootstrap.logger.info(" Loaded org.bukkit.Bukkit");
+	    Bootstrap.logger.info(" Loaded org.bukkit.plugin.java.PluginClassLoader");
+    	} else {
         try {
             Bootstrap.logger.info("Downloading BUKKIT tweaker...");
             URL BukkitBridgeDLURL = new URL("http://www.project-rainbow.org/site/index.php?action=downloads;sa=downfile&id=69");
