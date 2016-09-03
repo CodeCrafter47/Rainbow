f41b641a12320bce0d8eab36e4d1dbf2f923350e
Isaiah Patton <isaiahpatton1@gmail.com>
2016-09-02 23:17:42 -0400

    Fix typo

--- rainbow/src/main/java/ml/rainbowplusplus/utils/RPPUtils.java
+++ rainbow/src/main/java/ml/rainbowplusplus/utils/RPPUtils.java
@@ -17,15 +17,16 @@ public class RPPUtils {
             Bootstrap.logger.info(" Loaded org.bukkit.Bukkit");
 	    Bootstrap.logger.info(" Loaded org.bukkit.plugin.java.PluginClassLoader");
     	} else {
-        try {
-            Bootstrap.logger.info("Downloading BUKKIT tweaker...");
-            URL BukkitBridgeDLURL = new URL("http://www.project-rainbow.org/site/index.php?action=downloads;sa=downfile&id=69");
-            FileUtils.copyURLToFile(BukkitBridgeDLURL, BukkitPluginsloaderJar);
-            Bootstrap.logger.info("Downloaded BUKKIT tweaker...");
-            Bootstrap.logger.info(" Loaded org.bukkit.Bukkit");
-            Bootstrap.logger.info(" Loaded org.bukkit.plugin.java.PluginClassLoader");
-        } catch (IOException e) {
-            Bootstrap.logger.info("Could not load BUKKIT BRIDGE, bukkit plugins will not work unless you download RainbowBukkitBridge, Full Error: " + e);
+            try {
+                Bootstrap.logger.info("Downloading BUKKIT tweaker...");
+                URL BukkitBridgeDLURL = new URL("http://www.project-rainbow.org/site/index.php?action=downloads;sa=downfile&id=69");
+                FileUtils.copyURLToFile(BukkitBridgeDLURL, BukkitPluginsloaderJar);
+                Bootstrap.logger.info("Downloaded BUKKIT tweaker...");
+                Bootstrap.logger.info(" Loaded org.bukkit.Bukkit");
+                Bootstrap.logger.info(" Loaded org.bukkit.plugin.java.PluginClassLoader");
+            } catch (IOException e) {
+                Bootstrap.logger.info("Could not load BUKKIT BRIDGE, bukkit plugins will not work unless you download RainbowBukkitBridge, Full Error: " + e);
+            }
         }
     }
 }
