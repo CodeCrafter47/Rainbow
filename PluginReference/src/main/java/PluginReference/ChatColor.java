a568b4f5bfd1319b5b018fa0eed6f8651eb36a9f
Isaiah Patton <IsaiahPatton@users.noreply.github.com>
2016-11-08 16:58:34 -0500

    Formating fix
    I had Notepad++ set to replace tabs with spaces, but copying it to github broke

--- PluginReference/src/main/java/PluginReference/ChatColor.java
+++ PluginReference/src/main/java/PluginReference/ChatColor.java
@@ -4,30 +4,30 @@ package PluginReference;
  * Class containing color codes and related functions.
  */ 			
 public class ChatColor {
-	public static String ColorPrefix   = "\u00A7";
+        public static String ColorPrefix   = "\u00A7";
         public static char   ColorChar     = '\u00A7';
-	public static String BLACK	   = ColorPrefix + "0";
-	public static String DARK_BLUE 	   = ColorPrefix + "1";
-	public static String DARK_GREEN    = ColorPrefix + "2";
-	public static String DARK_AQUA 	   = ColorPrefix + "3";
-	public static String DARK_RED      = ColorPrefix + "4";
-	public static String DARK_PURPLE   = ColorPrefix + "5";
-	public static String GOLD          = ColorPrefix + "6";
-	public static String GRAY          = ColorPrefix + "7";
-	public static String DARK_GRAY     = ColorPrefix + "8";
-	public static String BLUE          = ColorPrefix + "9";
-	public static String GREEN         = ColorPrefix + "a";
-	public static String AQUA          = ColorPrefix + "b";
-	public static String RED           = ColorPrefix + "c";
-	public static String LIGHT_PURPLE  = ColorPrefix + "d";
-	public static String YELLOW        = ColorPrefix + "e";
-	public static String WHITE         = ColorPrefix + "f";
-	public static String MAGIC         = ColorPrefix + "k";
-	public static String BOLD          = ColorPrefix + "l";
-	public static String STRIKETHROUGH = ColorPrefix + "m";
-	public static String UNDERLINE     = ColorPrefix + "n";
-	public static String ITALIC        = ColorPrefix + "o";
-	public static String RESET         = ColorPrefix + "r";
+        public static String BLACK         = ColorPrefix + "0";
+        public static String DARK_BLUE     = ColorPrefix + "1";
+        public static String DARK_GREEN    = ColorPrefix + "2";
+        public static String DARK_AQUA     = ColorPrefix + "3";
+        public static String DARK_RED      = ColorPrefix + "4";
+        public static String DARK_PURPLE   = ColorPrefix + "5";
+        public static String GOLD          = ColorPrefix + "6";
+        public static String GRAY          = ColorPrefix + "7";
+        public static String DARK_GRAY     = ColorPrefix + "8";
+        public static String BLUE          = ColorPrefix + "9";
+        public static String GREEN         = ColorPrefix + "a";
+        public static String AQUA          = ColorPrefix + "b";
+        public static String RED           = ColorPrefix + "c";
+        public static String LIGHT_PURPLE  = ColorPrefix + "d";
+        public static String YELLOW        = ColorPrefix + "e";
+        public static String WHITE         = ColorPrefix + "f";
+        public static String MAGIC         = ColorPrefix + "k";
+        public static String BOLD          = ColorPrefix + "l";
+        public static String STRIKETHROUGH = ColorPrefix + "m";
+        public static String UNDERLINE     = ColorPrefix + "n";
+        public static String ITALIC        = ColorPrefix + "o";
+        public static String RESET         = ColorPrefix + "r";
         
 	
 	
