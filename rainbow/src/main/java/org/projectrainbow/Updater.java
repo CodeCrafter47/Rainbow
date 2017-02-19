33893bf3b68258760556c9e1deb0864293f19c8d
IsaiahPatton <isaiahpatton1@gmail.com>
2017-02-19 11:08:25 -0500

    Add requested changes

--- rainbow/src/main/java/org/projectrainbow/Updater.java
+++ rainbow/src/main/java/org/projectrainbow/Updater.java
@@ -3,10 +3,15 @@ package org.projectrainbow;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 
 import javax.net.ssl.SSLHandshakeException;
+import java.lang.NumberFormatException;
+
+import org.projectrainbow.launch.Bootstrap;
 
 /**
  * Check if Rainbow is outdated.
@@ -14,18 +19,38 @@ import javax.net.ssl.SSLHandshakeException;
  * @author Isaiah Patton
  */
 public class Updater {
+    public static String checkForUpdate() {
+        String latest = Updater.getLatestVersion();
+        if (!latest.startsWith("ERROR")) {
+            if (Bootstrap.buildNumber == latest) {
+                return "You are running the latest version.";
+            } else {
+                try {
+                    int behind = (Integer.valueOf(latest) - Integer.valueOf(Bootstrap.buildNumber));
+                    return "You are running " + behind + " versions behind.";
+                } catch (NumberFormatException e) {
+                    return "Unknown version, custom build?";
+                }
+            }
+        } else {
+            return latest;
+        }
+    }
+    
     public static String getLatestVersion() {
         try {
             return RawlatestVersion();
         } catch (SSLHandshakeException e) {
             return "ERROR: Update Java";
+        } catch (UnknownHostException e) {
+            return "ERROR: No internet";
         } catch (IOException e) {
             e.printStackTrace();
-            return "ERROR: java.io.IOException";
+            return "ERROR: " + e.getMessage();
         }
     }
-
-    public static String RawlatestVersion() throws SSLHandshakeException, IOException {
+    
+    public static String RawlatestVersion() throws SSLHandshakeException, UnknownHostException, MalformedURLException, IOException {
         URL url = new URL("https://ci.codecrafter47.de/job/Rainbow/lastStableBuild/buildNumber"); // Do latest stable build.
         URLConnection urlc = url.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(urlc.getInputStream(), "UTF-8"));
--- rainbow/src/main/java/org/projectrainbow/commands/_CmdVer.java
+++ rainbow/src/main/java/org/projectrainbow/commands/_CmdVer.java
@@ -1,17 +1,18 @@
 package org.projectrainbow.commands;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.projectrainbow.Updater;
+import org.projectrainbow._ColorHelper;
+import org.projectrainbow._DiwUtils;
+import org.projectrainbow.launch.Bootstrap;
+
 import PluginReference.MC_Player;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
-import org.projectrainbow._ColorHelper;
-import org.projectrainbow._DiwUtils;
-import org.projectrainbow.launch.Bootstrap;
-import org.projectrainbow.Updater;
-
-import java.util.Collections;
-import java.util.List;
 
 public class _CmdVer extends CommandBase {
     @Override
@@ -36,20 +37,8 @@ public class _CmdVer extends CommandBase {
 
     @Override
     public void execute(MinecraftServer minecraftServer, ICommandSender cs, String[] strings) throws CommandException {
-        _DiwUtils.reply(cs, String.valueOf(_ColorHelper.AQUA) + "Rainbow " + _ColorHelper.LIGHT_PURPLE + _DiwUtils.MC_VERSION_STRING + " b" + Bootstrap.buildNumber
-        );
+        _DiwUtils.reply(cs, String.valueOf(_ColorHelper.AQUA) + "Rainbow " + _ColorHelper.LIGHT_PURPLE + _DiwUtils.MC_VERSION_STRING + " b" + Bootstrap.buildNumber);
         _DiwUtils.reply(cs, "Checking for update, please wait...");
-        String latest = Updater.getLatestVersion();
-        if (!latest.startsWith("ERROR")) {
-            if (Bootstrap.buildNumber == latest) {
-                _DiwUtils.reply(cs, "You are running the latest version.");
-            } else {
-                int currentNum = Integer.valueOf(Bootstrap.buildNumber);
-                int latestNum = Integer.valueOf(latest);
-                _DiwUtils.reply(cs, "You are running " + (latestNum - currentNum) + " versions behind.");
-            }
-        } else {
-            _DiwUtils.reply(cs, "ERROR: Update to at least Java 8"); //Outdated java should be the only error (atm).
-        }
+        _DiwUtils.reply(cs, Updater.checkForUpdate());
     }
 }
