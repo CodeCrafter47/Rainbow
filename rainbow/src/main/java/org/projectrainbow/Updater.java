ac2d5c516681f0bca57d04ab92ea23f317136546
Isaiah <IsaiahPatton@users.noreply.github.com>
2017-02-20 10:17:44 -0500

    fix

--- rainbow/src/main/java/org/projectrainbow/Updater.java
+++ rainbow/src/main/java/org/projectrainbow/Updater.java
@@ -20,37 +20,32 @@ import org.projectrainbow.launch.Bootstrap;
  */
 public class Updater {
     public static String checkForUpdate() {
-        String latest = Updater.getLatestVersion();
+        String latest = getLatestVersion();
         if (!latest.startsWith("ERROR")) {
-            if (Bootstrap.buildNumber == latest) {
-                return "You are running the latest version.";
-            } else {
-                try {
-                    int behind = (Integer.valueOf(latest) - Integer.valueOf(Bootstrap.buildNumber));
-                    return "You are running " + behind + " versions behind.";
-                } catch (NumberFormatException e) {
-                    return "Unknown version, custom build?";
+            try {
+                int behind = (Integer.valueOf(latest) - Integer.valueOf(Bootstrap.buildNumber));
+                if (behind == 0) {
+                    return "You are running the latest version.";
                 }
+                return "You are running " + behind + " versions behind.";
+            } catch (NumberFormatException e) {
+                return "Unknown version, custom build?";
             }
         } else {
             return latest;
         }
     }
-    
+
     public static String getLatestVersion() {
         try {
             return RawlatestVersion();
-        } catch (SSLHandshakeException e) {
-            return "ERROR: Update Java";
-        } catch (UnknownHostException e) {
-            return "ERROR: No internet";
         } catch (IOException e) {
             e.printStackTrace();
             return "ERROR: " + e.getMessage();
         }
     }
-    
-    public static String RawlatestVersion() throws SSLHandshakeException, UnknownHostException, MalformedURLException, IOException {
+
+    public static String RawlatestVersion() throws IOException {
         URL url = new URL("https://ci.codecrafter47.de/job/Rainbow/lastStableBuild/buildNumber"); // Do latest stable build.
         URLConnection urlc = url.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(urlc.getInputStream(), "UTF-8"));
